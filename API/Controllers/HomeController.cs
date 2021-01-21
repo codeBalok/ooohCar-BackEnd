eea1fe9f67a435f28c0891917ae6ae2759ca0758
Sushil <askforsushil@gmail.com>
2021-01-21 23:42:28 +0530

    Updated API GetModelListForSideSearch and SideSearchGetMakeList in HomController

--- API/Controllers/HomeController.cs
+++ API/Controllers/HomeController.cs
@@ -35,30 +35,33 @@ namespace API.Controllers
         [Route("GetVehicleList")]
         public List<CommonViewModel> GetVehicleList()
         {
-            return _searchRepository.GetVehicleList().Select(x=> new CommonViewModel{
-                Id= x.Id,
-                Name = x.Name
-            }).ToList();
-        }
-    
-        [HttpGet]
-        [Route("GetLocationList")]
-        public List<CommonViewModel> GetLocationList()
-        {
-            return _searchRepository.GetLocationList().Select(x=> new CommonViewModel{
-                Id= x.Id,
+            return _searchRepository.GetVehicleList().Select(x => new CommonViewModel
+            {
+                Id = x.Id,
                 Name = x.Name
             }).ToList();
         }
 
-       
+        [HttpGet]
+        [Route("GetLocationList")]
+        public List<CommonViewModel> GetLocationList()
+        {
+            return _searchRepository.GetLocationList().Select(x => new CommonViewModel
+            {
+                Id = x.Id,
+                Name = x.Name
+            }).ToList();
+        }
+
+
 
         [HttpGet]
         [Route("GetMakeList")]
         public List<CommonViewModel> GetMakeList()
         {
-            return _searchRepository.GetMakeList().Select(x=> new CommonViewModel{
-                Id= x.Id,
+            return _searchRepository.GetMakeList().Select(x => new CommonViewModel
+            {
+                Id = x.Id,
                 Name = x.Name
             }).ToList();
         }
@@ -69,22 +72,26 @@ namespace API.Controllers
             return _searchRepository.GetMakeList().Select(x => new SideSearchCommonViewModel
             {
                 Id = x.Id,
-                Name = x.Name
-               // ChildCount= GetModelCountById(x.Id)
-            }).ToList();
+                Name = x.Name,
+                ChildCount = GetModelCountById(x.Id)
+
+            }).Where(x => x.ChildCount > 0).ToList();
         }
-        //public int GetModelCountById(int id)
-        //{
-        //    return _searchRepository.GetModalListCountByID(id).Count;
-        //}
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public int GetModelCountById(int id)
+        {
+            return _searchRepository.GetModalListCountByID(id).Count;
+        }
 
 
         [HttpGet]
         [Route("GetModelList/{makeId}")]
         public List<CommonViewModel> GetModelList(int makeId)
         {
-            return _searchRepository.GetModelList().Where(x=>x.MakeId == makeId).Select(x=> new CommonViewModel{
-                Id= x.Id,
+            return _searchRepository.GetModelList().Where(x => x.MakeId == makeId).Select(x => new CommonViewModel
+            {
+                Id = x.Id,
                 Name = x.Name
             }).ToList();
         }
@@ -97,7 +104,7 @@ namespace API.Controllers
             {
                 Id = x.Id,
                 Name = x.Name,
-                //ChildCount=GetVarientCountById(x.Id)
+                ChildCount = GetVarientCountById(x.Id)
             }).ToList();
         }
 
@@ -109,43 +116,87 @@ namespace API.Controllers
             {
                 Id = x.Id,
                 Name = x.Varient
-               
+
             }).ToList();
         }
-
-        //public int GetVarientCountById(int id)
-        //{
-        //    return _searchRepository.GetVarientListCountByID(id).Count;
-        //}
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public int GetVarientCountById(int id)
+        {
+            return _searchRepository.GetVarientListCountByID(id).Count;
+        }
 
         [HttpGet]
         [Route("GetYearList")]
         public List<CommonViewModel> GetYearList()
         {
-            return _searchRepository.GetYearList().Select(x=> new CommonViewModel{
-                Id= x.Id,
+            return _searchRepository.GetYearList().Select(x => new CommonViewModel
+            {
+                Id = x.Id,
                 Name = x.Name
             }).ToList();
         }
-    
+
         [HttpPost]
         [Route("GetSearchVehicleList")]
         public List<VehicleViewModel> GetSearchVehicleList([FromBody] SearchViewModel searchViewModel)
         {
-           return _searchRepository.GetSearchVehicleList(searchViewModel.CarTypeId, searchViewModel.MakeId, searchViewModel.CarModelId, searchViewModel.LocationId, searchViewModel.YearId).
-           Select(x=> new VehicleViewModel {
-               Id = x.Id,
-               Name = x.Name,
-               Vin = x.Vin,
-               Odometers = x.Odometers,
-               Image = _imageServiceRepository.GetImagesByModel(x.ModelId ?? 0),
-               Year = _searchRepository.GetYear(x.ModelId ?? 0),
-               Body = _searchRepository.GetBody(x.BodyTypeId),
-               FuelType = _searchRepository.GetFuelType(x.FuelTypeId ?? 0),
-               Transmission = _searchRepository.GetTransmission(x.TransmissionId ?? 0),
-               Cylinders = _searchRepository.GetCylinders(x.CylindersId ?? 0),
-               Type = _searchRepository.GetType(x.VehicalTypeId ?? 0),
-           }).ToList();
+            return _searchRepository.GetSearchVehicleList(searchViewModel.CarTypeId, searchViewModel.MakeId, searchViewModel.CarModelId, searchViewModel.LocationId, searchViewModel.YearId).
+            Select(x => new VehicleViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Vin = x.Vin,
+                Odometers = x.Odometers,
+                Image = _imageServiceRepository.GetImagesByModel(x.ModelId ?? 0),
+                Year = _searchRepository.GetYear(x.ModelId ?? 0),
+                Body = _searchRepository.GetBody(x.BodyTypeId),
+                FuelType = _searchRepository.GetFuelType(x.FuelTypeId ?? 0),
+                Transmission = _searchRepository.GetTransmission(x.TransmissionId ?? 0),
+                Cylinders = _searchRepository.GetCylinders(x.CylindersId ?? 0),
+                Type = _searchRepository.GetType(x.VehicalTypeId ?? 0),
+            }).ToList();
         }
+
+        [HttpPost]
+        [Route("GetVehicleListAccordingToSelectedMakes")]
+        public List<VehicleViewModel> GetVehicleListAccordingToSelectedMakes([FromBody] SelectedMakesListsViewModel selectedMakesListsViewModel)
+        {
+
+            List<int> makeIds = selectedMakesListsViewModel.Make.Select(makeId => makeId.Id).ToList();
+            return _searchRepository.GetVehicleListAccordingToSelectedMakes(makeIds).
+            Select(x => new VehicleViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Vin = x.Vin,
+                Odometers = x.Odometers,
+                Image = _imageServiceRepository.GetImagesByModel(x.ModelId ?? 0),
+                Year = _searchRepository.GetYear(x.ModelId ?? 0),
+                Body = _searchRepository.GetBody(x.BodyTypeId),
+                FuelType = _searchRepository.GetFuelType(x.FuelTypeId ?? 0),
+                Transmission = _searchRepository.GetTransmission(x.TransmissionId ?? 0),
+                Cylinders = _searchRepository.GetCylinders(x.CylindersId ?? 0),
+                Type = _searchRepository.GetType(x.VehicalTypeId ?? 0),
+            }).ToList();
+        }
+
+        //[HttpGet]
+        //[Route("SideSearchGetLocationListWithRegionCount")]
+        //public List<SideSearchCommonViewModel> SideSearchGetLocationListWithRegionCount()
+        //{
+        //    return _searchRepository.GetLocationList().Select(x => new SideSearchCommonViewModel
+        //    {
+        //        Id = x.Id,
+        //        Name = x.Name,
+        //        ChildCount = GetRegionCountById(x.Id)
+        //    }).Where(x => x.ChildCount > 0).ToList();
+        //}
+
+        //[ApiExplorerSettings(IgnoreApi = true)]
+        //public int GetRegionCountById(int id)
+        //{
+        //    return _searchRepository.GetRegionListByLocationId(id).Count;
+        //}
+
     }
 }
